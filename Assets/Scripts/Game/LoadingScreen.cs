ec49ed055f118af349ca2c47d97c828d5d6816b7
Sergey Belyaev <svbelyaev@avito.ru>
2025-03-16 14:57:18 +0500

    16 mar 2025, experiments of merge few menu in single scene

--- Assets/Scripts/Game/LoadingScreen.cs
+++ Assets/Scripts/Game/LoadingScreen.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,25 +6,46 @@ using UnityEngine.UI;
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField] private Slider progressBar;
+    
+    [SerializeField] private GameObject menu;
 
+    private IEnumerator coroutine;
     void Start()
     {
-        StartCoroutine(LoadMainMenuAsync());
+        coroutine = LoadMainMenuAsync();
+        StartCoroutine(coroutine);
     }
 
-    private System.Collections.IEnumerator LoadMainMenuAsync()
+    private IEnumerator LoadMainMenuAsync()
     {
         
-        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+        // AsyncOperation operation = SceneManager.LoadSceneAsync(1);
 
         
+        /*
         while (!operation.isDone)
         {
             if (progressBar != null)
             {
-                progressBar.value = operation.progress; 
+                progressBar.value = operation.progress;
             }
             yield return null;
+        }*/
+        
+        if (progressBar != null)
+        {
+            
+            while (progressBar.value < 100f)
+            {
+                progressBar.value += Time.deltaTime;
+                yield return null;
+            }
+            if (!Mathf.Approximately(progressBar.value, 100f))
+            {
+                progressBar.value += Time.deltaTime;
+                yield return new WaitForSeconds(0.5f);
+                
+            }
         }
     }
 }
